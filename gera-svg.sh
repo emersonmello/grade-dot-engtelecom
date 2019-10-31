09dac41a06457e3cc00d7a08d5b58e94f19bb9be
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2019-10-31 14:41:43 -0300

    gerando total de nós e arcos

--- gera-svg.sh
+++ gera-svg.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Emerson Ribeiro de Mello - 2019-10-18
 
-if ! [ $# -eq 2 ]; then
-    echo -e "Sintaxe errada. Informe o nome do arquivo de entrada DOT e o arquivo de saida SVG."
+if [ $# -lt 1 ]; then
+    echo -e "Sintaxe errada.\nInforme o nome do arquivo de entrada DOT e o arquivo de saida SVG."
     echo -e "Ex: $0 engtelecom.dot pagina-html/grade.svg\n"
+    echo -e "Ou, se desejar imprimir na tela informe o nome do arquivo de entrada DOT\n"
+    echo -e "Ex: $0 engtelecom.dot\n"
     exit 1
-fi
+fi 
 
 if [ ! -f preprocess.gvpr ]; then
     echo "Arquivo 'preprocess.gvpr' com regras de processamento não foi encontrado."
@@ -17,9 +19,14 @@ if [ ! -f "$1" ]; then
     exit 1
 fi 
 
-if [ "$1" = "$2" ]; then
-    echo "Nome do arquivo de saída não pode ser igual ao nome do arquivo de entrada"
-    exit 1
-fi 
+if [ $# -eq 2 ]; then
 
-gvpr "$1" -c -f preprocess.gvpr | dot -Tsvg > "$2"
+    if [ "$1" = "$2" ]; then
+        echo "Nome do arquivo de saída não pode ser igual ao nome do arquivo de entrada"
+        exit 1
+    fi 
+    gvpr "$1" -c -f preprocess.gvpr | dot -Tsvg > "$2"
+    exit 0
+fi
+
+gvpr "$1" -c -f preprocess.gvpr
