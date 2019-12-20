96b695318dae2d4383590936b52f5831b442a8d3
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2019-12-20 10:51:52 -0300

    correção do script

--- gera-sucessores-predecessores.sh
+++ gera-sucessores-predecessores.sh
@@ -38,7 +38,7 @@ fi
 mkdir -p "$2"
 
 # filtrando somente as linhas com ch= e removendo os espaços em branco que podem estar no início da linha
-disciplinas=`cat saida.dot | grep "ch=" |  sed -e 's/^[ \t]*//' | cut -d " " -f1`
+disciplinas=`cat $1 | grep "ch=" |  sed -e 's/^[ \t]*//' | cut -d " " -f1`
 
 for linha in $disciplinas; do
     gvpr "$1" -f "gvpr-scripts/supreprocess.gvpr" -a "$linha" | dot -Tpng > "$2/$linha.png"
